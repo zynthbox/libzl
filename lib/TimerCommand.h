edda1eb5cdc5c44e255efd6be0f94f132a166e90
Dan Leinir Turthra Jensen <admin@leinir.dk>
2022-07-12 15:02:39 +0100

    TimerCommand: Add Start/Stop clip operations to the operations enum

--- lib/TimerCommand.h
+++ lib/TimerCommand.h
@@ -12,6 +12,8 @@ struct TimerCommand {
         StopPlaybackOperation = 1, ///@< Stop all playback
         StartPartOperation = 2, ///@< Start playing the given part. Pass track index as parameter 1, sketch index as parameter2 and part index as parameter3
         StopPartOperation = 4, ///@< Stop playing the given part. Pass track index as parameter 1, sketch index as parameter2 and part index as parameter3
+        StartClipLoopOperation = 6, ///@< Start playing a clip looped, parameter being the midi channel, parameter2 being the clip ID, and parameter3 being the note
+        StopClipLoopOperation = 7, ///@< Stop playing a clop looping style, parameter being the midi channel to stop it on, parameter2 being the clip ID, and parameter3 being the note
     };
     Operation operation{InvalidOperation};
     int parameter{0};
