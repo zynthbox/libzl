d3d7a2bc9eec92d75f32e100b99582a8505a09a6
Dan Leinir Turthra Jensen <admin@leinir.dk>
2022-07-15 09:29:50 +0100

    TimerCommand: Add further parameters (extra int, one quint64)

--- lib/TimerCommand.h
+++ lib/TimerCommand.h
@@ -12,13 +12,15 @@ struct TimerCommand {
         StopPlaybackOperation = 1, ///@< Stop all playback
         StartPartOperation = 2, ///@< Start playing the given part. Pass track index as parameter 1, sketch index as parameter2 and part index as parameter3
         StopPartOperation = 4, ///@< Stop playing the given part. Pass track index as parameter 1, sketch index as parameter2 and part index as parameter3
-        StartClipLoopOperation = 6, ///@< Start playing a clip looped, parameter being the midi channel, parameter2 being the clip ID, and parameter3 being the note
+        StartClipLoopOperation = 6, ///@< Start playing a clip looped, parameter being the midi channel, parameter2 being the clip ID, and parameter3 being the note, and bigParameter can be used to define a timer offset value for adjusting the part's playback position relative to the timer's cumulative beat
         StopClipLoopOperation = 7, ///@< Stop playing a clop looping style, parameter being the midi channel to stop it on, parameter2 being the clip ID, and parameter3 being the note
     };
     Operation operation{InvalidOperation};
     int parameter{0};
     int parameter2{0};
     int parameter3{0};
+    int parameter4{0};
+    quint64 bigParameter{0};
     // NOTE: To implementers: Use this sparingly, as QVariants can be expensive to use and this gets handled from a jack call
     QVariant variantParameter;
 };
