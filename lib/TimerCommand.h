fed7e9c1c5cc564cffe26c3e088374fce8651dff
Dan Leinir Turthra Jensen <admin@leinir.dk>
2022-09-13 12:04:05 +0100

    TimerCommand: Add op for setting samplersynth channel enabled state

--- lib/TimerCommand.h
+++ lib/TimerCommand.h
@@ -16,6 +16,7 @@ struct TimerCommand {
         StopPartOperation = 4, ///@< Stop playing the given part. Pass channel index as parameter 1, track index as parameter2 and part index as parameter3
         StartClipLoopOperation = 6, ///@< Start playing a clip looped, parameter being the midi channel, parameter2 being the clip ID, and parameter3 being the note, and bigParameter can be used to define a timer offset value for adjusting the part's playback position relative to the timer's cumulative beat
         StopClipLoopOperation = 7, ///@< Stop playing a clop looping style, parameter being the midi channel to stop it on, parameter2 being the clip ID, and parameter3 being the note
+        SamplerChannelEnabledStateOperation = 8, ///@< Sets the state of a SamplerSynth channel to enabled or not enabled. parameter is the sampler channel (-2 through 9, -2 being uneffected global, -1 being effected global, and 0 through 9 being zl channels), and parameter2 is 0 for disabled, any other number for enabled
         RegisterCASOperation = 10001, ///@< INTERNAL - Register a ClipAudioSource with SamplerSynth, so it can be used for playback - variantParameter should contain a ClipAudioSource* object instance
         UnregisterCASOperation = 10002, ///@< INTERNAL - Unregister a ClipAudioSource with SamplerSynth, so it can be used for playback - variantParameter should contain a ClipAudioSource* object instance
     };
